2e82f1e9426cef9dd45de6d5bb954db5c2ee1f18
jose-cassios <cassios.torres.010@gmail.com>
2026-02-09 22:08:09 -0300

    feat: add date input

--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,11 +1,25 @@
 import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
 import { Button } from "../../../components/button";
+// import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
 interface DestinationAndDateStepProps {
     isGuestsInputOpen: boolean
     closeGuestsInput: () => void
     openGuestsInput: () => void
 }
+
+// export default function BasicTimePicker() {
+//     return (
+//       <LocalizationProvider dateAdapter={AdapterDayjs}>
+//         <DemoContainer components={['TimePicker']}>
+//           <TimePicker label="Basic time picker" />
+//         </DemoContainer>
+//       </LocalizationProvider>
+//     );
+//   }
 
 export function DestinationAndDateStep({closeGuestsInput, isGuestsInputOpen, openGuestsInput}: DestinationAndDateStepProps){
     return (
--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -35,7 +35,7 @@ export function CreateActivityModal ({
                     <div className="flex items-center gap-2">
                         <div className="h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
                             <Calendar className="text-zinc-400 size-5" />
-                            <input type="datetime-local" name="occurs_at" placeholder="Data e horário da atividade" className="bg-transparent [color-scheme:dark] text-lg placeholder-zinc-400 outline-none flex-1"/>
+                            <input type="date" name="occurs_at" placeholder="Data e horário da atividade" className="bg-transparent [color-scheme:dark] text-lg placeholder-zinc-400 outline-none flex-1"/>
                         </div>
                     </div>
 
